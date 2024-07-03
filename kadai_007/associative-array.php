5ca9dd2bf2c321a2ae0d01e43603f9ffd8c71512
shin-34 <shinmegg@gmail.com>
2024-07-04 08:27:08 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $vegetable_date = ['name' => 'onion', 'price' => 200, 'weight' => 160,];
+
+         print_r($vegetable_date);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
